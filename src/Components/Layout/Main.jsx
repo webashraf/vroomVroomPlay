67f63e2482ea4296619053959358590243e24691
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2023-05-17 23:36:53 +0600

    add home page and footer on the project

---
+++ src/Components/Layout/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../shared/Navbar/Navbar';
+import Footer from '../shared/Footer/Footer';
+import { Outlet } from 'react-router-dom';
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
