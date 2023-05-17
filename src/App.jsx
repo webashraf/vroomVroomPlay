60c333ad5b3e74e87cd8db6c72bccf89a81226ae
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2023-05-17 23:05:14 +0600

    connect a-11 client side on the private repo

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <button className="btn btn-active">Button</button>
+      <button className="btn btn-active btn-primary">Button</button>
+      <button className="btn btn-active btn-secondary">Button</button>
+      <button className="btn btn-active btn-accent">Button</button>
+      <button className="btn btn-active btn-ghost">Button</button>
+      <button className="btn btn-active btn-link">Button</button>
+    </>
+  );
+}
+
+export default App;
