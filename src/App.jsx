a672889e56a0548c7561be3c1bf74967817af9c2
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2023-05-17 23:30:22 +0600

    add navbar on the page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Navbar from "./Components/shared/Navbar/Navbar";
 
 function App() {
 
   return (
     <>
+    <Navbar></Navbar>
       <button className="btn btn-active">Button</button>
       <button className="btn btn-active btn-primary">Button</button>
       <button className="btn btn-active btn-secondary">Button</button>
