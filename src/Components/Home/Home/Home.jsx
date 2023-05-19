2151998c90c948de8cef17ad2b39b8e8393c2230
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2023-05-19 13:26:03 +0600

    add gallery demo

--- src/Components/Home/Home/Home.jsx
+++ src/Components/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import ShopByCategory from '../ShopByCategory/ShopByCategory';
+import CarGallery from '../../CarGallery/CarGallery';
 
 const Home = () => {
     return (
         <div className='flex flex-col gap-20'>
             <Banner></Banner>
             <ShopByCategory></ShopByCategory>
+            <CarGallery></CarGallery>
         </div>
     );
 };
